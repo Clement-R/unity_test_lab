d2645f054f62101637c99663a197d1d37c2f1225
Scarounet <rondeau.clement@gmail.com>
2015-11-30 08:45:19 +0100

    Init, working proof of concept

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+
+    private int level = 1;
+
+	// Use this for initialization
+	void Start () {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+
+    void InitGame() {
+        boardScript.SetupScene(level);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}

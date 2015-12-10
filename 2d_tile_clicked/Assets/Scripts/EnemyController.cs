8d4ff465ff84d6214d939dcf13959187cb45ee57
Scarounet <rondeau.clement@gmail.com>
2015-12-10 07:32:24 +0100

    Add new script for enemy movement, put some debug in it to get sorting layer name

---
+++ 2d_tile_clicked/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		Debug.Log (sr.sortingLayerName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

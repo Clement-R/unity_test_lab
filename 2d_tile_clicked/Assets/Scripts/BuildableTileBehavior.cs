21b854471f6c30fc37e776a3090271237e123681
Clement-R <rondeau.clement@gmail.com>
2015-12-15 13:20:25 +0100

    change comment

--- 2d_tile_clicked/Assets/Scripts/BuildableTileBehavior.cs
+++ 2d_tile_clicked/Assets/Scripts/BuildableTileBehavior.cs
@@ -5,12 +5,12 @@ public class BuildableTileBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnMouseDown() {
@@ -19,7 +19,7 @@ public class BuildableTileBehavior : MonoBehaviour {
 		Debug.Log (this.transform.position.y);
 		Vector3 position = new Vector3 (this.transform.position.x, this.transform.position.y, 0f);
 
-		// Instantiates a prefab named "Tower" located in any Resources folder in your project's Assets folder.
+		// Instantiates a prefab named "Tower" located in any Resources folder in the project's Assets folder.
 		Instantiate(Resources.Load("Tower", typeof(GameObject)), position, Quaternion.identity);
 	}
 }

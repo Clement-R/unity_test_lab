06e911f653a3051630cb00577177ae7b5292eaa1
Clement-R <rondeau.clement@gmail.com>
2015-12-15 14:15:15 +0100

    Add enemy prefab and new sorting layout

--- 2d_tile_clicked/Assets/Scripts/EnemyController.cs
+++ 2d_tile_clicked/Assets/Scripts/EnemyController.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+	public Vector2 velocity;
+    private Rigidbody2D rb2D;
+
 	// Use this for initialization
 	void Start () {
 		SpriteRenderer sr = GetComponent<SpriteRenderer>();
 		Debug.Log (sr.sortingLayerName);
+		rb2D = GetComponent<Rigidbody2D>();
 	}
 
-	// Update is called once per frame
-	void Update () {
+    void FixedUpdate() {
+        // rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+    }
 
+	void SearchForNextTile() {
+	}
+
+	void Move() {
 	}
 }

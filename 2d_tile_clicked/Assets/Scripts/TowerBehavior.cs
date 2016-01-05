ac7aa7c33b204acd81197bf9200450e08a81d5a4
Scarounet <rondeau.clement@gmail.com>
2016-01-05 07:15:31 +0100

    Add bullet prefab, and fix some errors in TowerBehavior script

--- 2d_tile_clicked/Assets/Scripts/TowerBehavior.cs
+++ 2d_tile_clicked/Assets/Scripts/TowerBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TowerBehavior : MonoBehaviour {
 
@@ -14,8 +15,8 @@ public class TowerBehavior : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (enemiesInRange.Count() > 0) {
-			// Focus enemy that is the closest to the exit
+		if (enemiesInRange.Count > 0) {
+			// Focus enemy that has enter the area first (FIFO list)
 			// TODO : Make a way to change behavior
 			Vector3 enemyPos = enemiesInRange[0].transform.position;
 			transform.rotation = Quaternion.LookRotation(Vector3.forward, enemyPos - transform.position);
@@ -29,7 +30,8 @@ public class TowerBehavior : MonoBehaviour {
     }
 
 	void OnTriggerExit2D(Collider2D enemy) {
-		enemiesInRange.Remove(enemy);
+		aimedEnemy = enemy.gameObject;
+		enemiesInRange.Remove(aimedEnemy);
 		// TODO : Debug to ensure that the enemy removed is the one that's passed in param
 		//        and not a random one. C#.
 	}

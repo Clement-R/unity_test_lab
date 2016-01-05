4df9f1bce6ddbf002bbdbf40e837ee261acd9a8d
Clement-R <rondeau.clement@gmail.com>
2016-01-05 22:57:57 +0100

    Add movement on bullet and destroy it 2s after creation

---
+++ 2d_tile_clicked/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+    public float speed = 10;
+
+    void Start() {
+        Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(0, speed * Time.deltaTime, 0);
+	}
+}

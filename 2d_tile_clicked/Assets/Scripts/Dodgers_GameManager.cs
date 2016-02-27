0c2451b3632b35678bf28dfed1fa3c0798d70276
Clement-R <rondeau.clement@gmail.com>
2016-02-27 11:52:58 +0100

    Add UFO tutorial from Unity official tutorial. Add movement in Dodgers game

---
+++ 2d_tile_clicked/Assets/Scripts/Dodgers_GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class Dodgers_GameManager : MonoBehaviour {
+    public GameObject[] asteroids;
+    public float fallSpeed = 2f;
+    private List<GameObject> inGameAsteroids = new List<GameObject> ();
+    private float timeBetweenAsteroid = 2.0f;
+
+    // Use this for initialization
+	void Start () {
+        StartCoroutine(LaunchAsteroids());
+	}
+    
+    IEnumerator LaunchAsteroids() {
+        while (true) {
+            // TODO : Let asteroids come from all sides of the screen
+
+            // x between 7 and -7
+            float x = Random.Range(-7, 7);
+            // y fixed to 8
+            float y = 8f;
+
+            GameObject toInstantiate = asteroids[Random.Range(0, asteroids.Length)];
+            GameObject asteroid = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+            Rigidbody2D rb2 = asteroid.GetComponent<Rigidbody2D>();
+            rb2.velocity = new Vector2(0f, -fallSpeed);
+            inGameAsteroids.Add(asteroid);
+            yield return new WaitForSeconds(timeBetweenAsteroid);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
---
+++ 2d_tile_clicked/Assets/Scripts/Dodgers_PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Dodgers_PlayerController : MonoBehaviour {
+    public float speed = 30.0f;
+    private Rigidbody2D rb2d;
+
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+    }
+}
